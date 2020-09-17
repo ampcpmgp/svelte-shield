def2df8d033c852423b1e4d9caea028e77715de1
ampcpmgp <ampcpmgp@gmail.com>
2020-09-17 14:21:09 +0900

    漢字のみの動詞は繰り上げる

--- src/states/morpheme.js
+++ src/states/morpheme.js
@@ -156,6 +156,10 @@ export function composite(path) {
       nextItem.pos === '名詞'
     ) {
       ++currentIndex
+
+      // 動詞ではあるものの、それが漢字のみで構成されていた場合、後ろに結合させる。
+    } else if (item.pos === '動詞' && isOnlyKanji(item.surface_form)) {
+      ++currentIndex
     }
 
     // 事前折り返し判定により再初期化
