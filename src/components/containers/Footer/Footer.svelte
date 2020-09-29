56a4435cd6677f0a0525a74045915da56bb009c8
ampcpmgp <ampcpmgp@gmail.com>
2020-09-29 23:50:44 +0900

    プロダクションビルド時のみカウンター表示

--- src/components/containers/Footer/Footer.svelte
+++ src/components/containers/Footer/Footer.svelte
@@ -11,11 +11,15 @@
 </style>
 
 <div class="wrapper">
-  <a href="https://hits.seeyoufarm.com" target="_blank" rel="noopener">
-    <img
-      src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fampcpmgp.github.io%2Fsvelte-shield%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Page+View&edge_flat=false"
-      alt="" />
-  </a>
+  {#if process.env.NODE_ENV === 'production'}
+    <a href="https://hits.seeyoufarm.com" target="_blank" rel="noopener">
+      <img
+        src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fampcpmgp.github.io%2Fsvelte-shield%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Page+View&edge_flat=false"
+        alt="" />
+    </a>
+  {:else}
+    <div />
+  {/if}
 
   <Version />
 </div>
