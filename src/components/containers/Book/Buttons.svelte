56e741769def8f0c89ca8273a3f94eeb42d3c62f
ampcpmgp <ampcpmgp@gmail.com>
2020-11-16 10:34:24 +0900

    ボタン位置調整

--- src/components/containers/Book/Buttons.svelte
+++ src/components/containers/Book/Buttons.svelte
@@ -2,6 +2,14 @@
 </script>
 
 <style>
+  .wrapper {
+    display: grid;
+    place-items: center;
+    grid-auto-flow: column;
+    grid-column-gap: 8px;
+    margin: auto;
+  }
+
   .all-install {
     color: royalblue;
   }
@@ -11,7 +19,7 @@
   }
 </style>
 
-<div>
+<div class="wrapper">
   <button class="btn btn-sm btn-primary all-install">一括取込</button>
   <button class="btn btn-sm btn-primary new-register">新規登録</button>
 </div>
