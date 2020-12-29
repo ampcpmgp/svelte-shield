b005dea450d8d2fbea22287fe980577b58128868
ampcpmgp <ampcpmgp@gmail.com>
2020-12-29 17:40:03 +0900

    ファイル名変更

--- src/states/morpheme-composite/数値区切りカンマ.test.js
+++ src/states/morpheme-composite/数値区切りカンマ.test.js
@@ -1,12 +1,12 @@
 import test from 'ava'
-import json from './小数点の判定.json'
+import json from './数値区切りカンマ.json'
 import { composite } from '../morpheme'
 
 /* 原文
 北極の六角形構造は、直線部の一辺が地球の直径を越える長さ約13,800kmである[52]。
 */
 
-test('composite/小数点の判定', t => {
+test('composite/数値区切りカンマ', t => {
   const result = composite(json).map(item => item.word)
 
   t.deepEqual(result, [
