88cea8c14dc1f27fd18e75714d7e76622519b30b
ampcpmgp <ampcpmgp@gmail.com>
2021-04-18 22:11:17 +0900

    fix env error & 2021.04.18.2210

--- src/components/parts/Footer/Footer.svelte
+++ src/components/parts/Footer/Footer.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="wrapper">
-  {#if import.meta.env.PROD === "production"}
+  {#if import.meta.env.PROD}
     <a href="https://hits.seeyoufarm.com" target="_blank" rel="noopener">
       <img
         src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fampcpmgp.github.io%2Fsvelte-shield%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Page+View&edge_flat=false"
