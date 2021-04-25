19a62b2afbc6d32283d9e670d15ad30e248a1219
ampcpmgp <ampcpmgp@gmail.com>
2021-04-25 15:26:05 +0900

    開発環境更新

--- vite-ext-tab.config.js
+++ vite-ext-tab.config.js
@@ -5,15 +5,10 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  root: resolve(__dirname, "chrome-extension"),
   publicDir: "no-public-dir",
   build: {
     target: "es2018",
-    outDir: "chrome-extension/dist/content",
-    lib: {
-      formats: ["umd"],
-      entry: resolve(__dirname, "chrome-extension/content.js"),
-      name: "svelteShield",
-      fileName: "content",
-    },
+    outDir: "dist/tab",
   },
 });
