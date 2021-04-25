5fcf1d7823b202aac055173c47e25a2a714d7fbd
ampcpmgp <ampcpmgp@gmail.com>
2021-04-25 16:05:10 +0900

    開発環境を元に戻す

---
+++ chrome-extension/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { test } from "./state";
+
+  export let removeApp = () => {};
+</script>
+
+<div class="background" on:click={removeApp} />
+
+<div class="content">{$test}</div>
+
+<style>
+  :global(#svelte-shield-chrome-extension-app-1234567890abcde) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999999999999999999999999999;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: grid;
+    place-content: center;
+  }
+
+  .content {
+    position: relative;
+    width: 80vw;
+    max-width: 1280px;
+    height: 80vh;
+    background-color: lightyellow;
+  }
+
+  .background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+  }
+</style>
--- vite-ext-content.config.js
+++ vite-ext-content.config.js
@@ -5,10 +5,15 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  root: resolve(__dirname, "chrome-extension"),
   publicDir: "no-public-dir",
   build: {
     target: "es2018",
-    outDir: "dist/tab",
+    outDir: "chrome-extension/dist/content",
+    lib: {
+      formats: ["umd"],
+      entry: resolve(__dirname, "chrome-extension/content.js"),
+      name: "svelteShield",
+      fileName: "content",
+    },
   },
 });
