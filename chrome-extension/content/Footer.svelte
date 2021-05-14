137a70741707f50fbbba5a420d52f65a2f80c677
ampcpmgp <ampcpmgp@gmail.com>
2021-05-14 21:14:48 +0900

    ウェブサイトのリンクを添付

--- chrome-extension/content/Footer.svelte
+++ chrome-extension/content/Footer.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <div class="SVELTESHIELD-wrapper">
-  <a target="_blank" href="TODO: URL">
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://chrome.google.com/webstore/detail/svelte-shield-%E6%8B%A1%E5%BC%B5%E6%A9%9F%E8%83%BD/ogdlcilfgiojgaoglhhilbhnghchnceo"
+  >
     <img src={WebStorePng} alt="Chrome Web Store" />
   </a>
 </div>
