ee26463b98667ec406da572edf52ad26bd34655d
ampcpmgp <ampcpmgp@users.noreply.github.com>
2021-07-27 12:03:48 +0000

    deploy: ecdf268f4ea9bd9648c1b50420a138a0dab63989

--- assets/main.4e6ead1c.js
+++
@@ -1,1 +0,0 @@
-import"./dynamic-import-polyfill.39257c8e.js";import"./main.eac15099.js";import"./vendor.5a9d8d9d.js";
---
+++ assets/mock.300b4256.js
@@ -0,0 +1,1 @@
+import"./dynamic-import-polyfill.39257c8e.js";import{a7 as d}from"./vendor.5a9d8d9d.js";import"./main.dde49332.js";d();
